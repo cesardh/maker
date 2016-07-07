ea424990e294a3f4868fcd094ddc2a36e0ff059d
cesardh <cesardh777@gmail.com>
2016-07-06 20:09:41 -0500

    avance

--- retos-jquery/ListaTODO/jquery.js
+++ retos-jquery/ListaTODO/jquery.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
+  var tarea;
+  var lista;
+
   $('#input').keydown(function(event) {
     if (event.keyCode == '13' ) {
-      var tarea = $('#input').val();
-      $('.todo-list').append('<li>' + tarea + '</li>');
+      var checkbox = '<input type="checkbox">';
+      var borrar = '<span class="equis">X</span>'
+      tarea = $('#input').val();
+
+      $('.todo-list').append('<div class="lista">' + checkbox + tarea + borrar + '</div>');
+      $('#input').val("");
+      $('span').hide();
     };
   });
+
+  $('.todo-list').on('mouseenter', '.lista', function() {
+    $(this).css('background-color', '#DDD');
+    $('.equis').show();
+
+  }).on('mouseleave', '.lista', function(){
+    $(this).css('background-color', 'white');
+    $(this).has('.equis').hide('1000');
+  });
+
 });
