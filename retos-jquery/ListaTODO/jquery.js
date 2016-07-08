e97830645014bd2221227583b981cf875e38d479
cesardh <cesardh777@gmail.com>
2016-07-08 17:56:03 -0500

    todo-list finalizado

--- retos-jquery/ListaTODO/jquery.js
+++ retos-jquery/ListaTODO/jquery.js
@@ -1,26 +1,55 @@
 $(document).ready(function() {
   var tarea;
   var lista;
+  var borrar;
 
+//Crear tareal al presionar ENTER
   $('#input').keydown(function(event) {
     if (event.keyCode == '13' ) {
-      var checkbox = '<input type="checkbox">';
-      var borrar = '<span class="equis">X</span>'
+      var checkbox = '<input type="checkbox" class="checkbox">';
+      borrar = '<span class="borrar"></span>';
       tarea = $('#input').val();
 
-      $('.todo-list').append('<div class="lista">' + checkbox + tarea + borrar + '</div>');
+      $('.todo-list').append('<li class="lista">' + checkbox + tarea + borrar + '</li>');
       $('#input').val("");
-      $('span').hide();
     };
   });
 
+//Estilo de la lista cuando el mouse esta dentro
   $('.todo-list').on('mouseenter', '.lista', function() {
-    $(this).css('background-color', '#DDD');
-    $('.equis').show();
 
+    $(this).css('background-color', '#DDD');
+    $('.borrar', this).html('X').show()
+    .css({
+      fontSize: '25px',
+      float: 'right',
+      cursor: 'default'
+    });
+
+
+//Estilo de la lista cuando el mouse esta fuera
   }).on('mouseleave', '.lista', function(){
     $(this).css('background-color', 'white');
-    $(this).has('.equis').hide('1000');
+    $('.borrar', this).hide();
+
+//Borrar tarea sin presionamos la "X"
+  }).on('click', '.borrar', function() {
+    $(this).parent().remove();
+
+//Cabiar Css cuando el checkbox esté seleccionado
+  }).on('click', '.checkbox', function() {
+    if (this.checked) {
+      $(this).parent().css({
+        color: 'gray',
+        textDecoration: 'line-through'
+      });
+    }else{
+      $(this).parent().css({
+        color: '#666',
+        textDecoration: 'none'
+      });
+    };
   });
+
 
 });
