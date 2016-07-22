657190dc5828efe5a09303b572f0a0610454f4c7
cesardh <cesardh777@gmail.com>
2016-07-22 18:17:27 -0500

    primer commit

---
+++ rails/prueba/app/views/personas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @persona, :id, :nombre, :apellido, :cesdula, :created_at, :updated_at
--- retos_ruby/retos.rb
+++ retos_ruby/retos.rb
@@ -27,25 +27,61 @@ def fizzbuzz num
   return num
 end
 
+#reto 6: MyCar
+class MyCar
+  def initialize(año, color, modelo)
+    @año = año
+    @color = color
+    @modelo = modelo
+    @velocidad_actual = 0
+
+  end
+
+  def acelerar velocidad
+    @velocidad_actual += velocidad
+  end
+
+  def frenar
+    des = @velocidad_actual/10
+    while @velocidad_actual != 0
+      @velocidad_actual -= des
+      p @velocidad_actual
+
+    end
+  end
+
+  def apagar
+    while @velocidad_actual != 0
+      @velocidad_actual -= 1
+      p @velocidad_actual
+    end
+  end
+
+end
+
 #Programa de entrada
 mensaje = <<-MSG
 Hola usuario
 Ejecuta el metodo correspondiente al reto con los paremetros indicados
 
   Reto 1: Suma
-    suma( valor1, valor2)
+    -suma( valor1, valor2)
 
   Reto 2: Saludar
-    saluda("tu nombre")
+    -saluda("tu nombre")
 
   Reto 3: Longitud de cadena
-    longitud("cadena de texto")
+    -longitud("cadena de texto")
 
   Reto 4: Iterando sobre arreglo
-    contar( [arrego de números] , numero)
+    -contar( [arrego de números] , numero)
 
   Reto 5: FizzBuzz
-    fizzbuzz(valor)
+    -fizzbuzz(valor)
+
+  Reto 6: MyCar
+    -Crear instancia de la clase MyCar(año, color, modelo)
+    -Ejecutar métodos: acelear( velocidad ), frenar(), apagar()
 
 MSG
 
