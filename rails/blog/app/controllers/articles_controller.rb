9781d7a8a4b831dd250bf2accf6454cd21c0d522
César Duarte <cesardh777@gmail.com>
2016-08-01 14:36:03 -0500

    cambios

--- rails/blog/app/controllers/articles_controller.rb
+++ rails/blog/app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
 
+	def index
+	  @articles = Article.all
+	end
+
+
 	def new
 		@article = Article.new
 	end
 
-	
 
 	def create
 
@@ -19,15 +23,8 @@ class ArticlesController < ApplicationController
 	end
 
 
-
 	def show
 	  @article = Article.find(params[:id])
-	end
-
-
-
-	def index
-	  @articles = Article.all
 	end
 
 
@@ -41,7 +38,14 @@ class ArticlesController < ApplicationController
 	  end
 	end
 
-	
+	def destroy
+
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
 
 	def edit
 	  @article = Article.find(params[:id])
